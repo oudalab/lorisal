998f15e030da26c2db69976d95da759df9ceeb3f
Daniel Helm <daniel.helm@ou.edu>
2017-01-26 12:59:35 -0600

    Added pycharm folder to gitignore and changed setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'lorisal',
     version = '0.1',
     author= 'OU Data Lab',
-    authour_email='daniel.helm@ou.edu',
+    author_email='daniel.helm@ou.edu',
     packages=find_packages(exclude=('tests', 'docs'))
 )
 
